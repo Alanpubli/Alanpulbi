7e06903a1a5b536681d9966823be101362fe35fd
Alan David <alandavid@gmx.net>
2019-04-10 12:04:07 +0200

    Change

--- mail.php
+++ mail.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html>
+<meta charset="utf-8">
+<title>Forms action page</title>
+<link rel="stylesheet" href="/w3css/4/w3.css">
+<body class="w3-container">
+
+
 <?php $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
@@ -8,3 +16,7 @@ $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You!";
 ?>
+
+
+</body>
+</html>
